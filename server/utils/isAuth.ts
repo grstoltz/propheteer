336b7110160a30f5a46e84f5956c3259f73f09a6
Grant Stoltz <grant@grantstoltz.com>
2020-10-17 14:45:09 -0700

    fixing tsc errors

--- server/utils/isAuth.ts
+++ server/utils/isAuth.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 
 export const isAuth = (req: any, res: Response, next: NextFunction): void => {
 	if (!req.session?.token) {
